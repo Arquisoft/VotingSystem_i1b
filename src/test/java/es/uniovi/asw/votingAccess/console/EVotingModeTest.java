0ed4f6c5c950705e36584d1b549f065daf06e755
Inmaculada Martínez Lobo <uo238739@uniovi.es>
2016-04-20 19:08:20 +0200

    Added class to fill production database

---
+++ src/test/java/es/uniovi/asw/votingAccess/console/EVotingModeTest.java
@@ -0,0 +1,17 @@
+package es.uniovi.asw.votingAccess.console;
+
+import org.junit.Test;
+
+public class EVotingModeTest {
+
+	@Test
+	public void testRegisterEVoterAction() {
+		//TODO
+	}
+
+	
+	@Test
+	public void testLogInAndVoteAction(){
+		//TODO
+	}
+}
---
+++ src/test/java/es/uniovi/asw/votingAccess/console/ElectoralBoardModeTest.java
@@ -0,0 +1,18 @@
+package es.uniovi.asw.votingAccess.console;
+
+import org.junit.Test;
+
+public class ElectoralBoardModeTest {
+
+	@Test
+	public void testMarkVoterAction() {
+		//TODO
+	}
+
+	
+	@Test
+	public void testSubmitVoteAction(){
+		//TODO
+	}
+
+}
